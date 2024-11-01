cec566e612731c71be5171835523f454c1bac91a
Nick-Major <divanchik26@gmail.com>
2024-11-01 13:09:16 +0300

    Last commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
-export const character = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
-export let order = ['name', 'level'];
+const character = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
+let order = ['name', 'level'];
 
-export function customSorting(obj, arr) {
-    let objProps = [];
-  
-    Object.keys(obj).forEach((key) => {
-      if (!arr.includes(key)) {
-        objProps.push(key);
-      }
-    });
+function orderByProps(obj, arr) {
+  let arrKeys = Object.entries(obj);
 
-    objProps.sort(function(a, b) {
-        if(a < b) {
-            return -1;
+  arrKeys.sort(function(a, b) {
+      let aStr = a[0];
+      let bStr = b[0];
+      let hasAstr = arr.includes(aStr);
+      let hasBstr = arr.includes(bStr);
+      
+      if(hasAstr && !hasBstr) {//если есть совпадение с массивом в A и нету в B, то ставим а слева
+        return -1;
+      } else if (hasBstr && !hasAstr) {//если есть совпадение с массивом в B и нету в A, то ставим b слева
+        return 1;
+      } else if (hasAstr && hasBstr) {//если есть совпадение с массивом в A и B, то надо получить индексы A и B, далее сравнить индексы и поставить слева тот что меньше
+        let indexA = arr.indexOf(aStr);
+        let indexB = arr.indexOf(bStr);
+        if (indexA < indexB) {
+          return -1;
+        } else if (indexA > indexB) {
+          return 1;
         } else {
-            return 1;
+          return 0;
         }
-    })
+      } else {//если нет совпадений нужно сортировать A и B по умолчанию: если A меньше B то возращаем -1, если A больше B то возращаем 1, если A и B равны то возращаем 0
+        if(aStr < bStr) {
+          return -1;
+        } else if (aStr > bStr) {
+          return 1;
+        } else {
+          return 0;
+        }
+      };
+  });
 
-    objProps = [...arr, ...objProps];
-    return objProps.map((key) => ({ key, value: obj[key] }));
+  return arrKeys;
 };
-  
-console.log(customSorting(character, order));
+
+console.log(orderByProps(character, order));
+
